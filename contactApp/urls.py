3b5c7424d07a8b69bd8b9748b3364439c5e9ec79
Abdullahi Salawu <salabdullah33@gmail.coom>
2022-07-28 16:17:07 +0100

    contact app

--- contactApp/urls.py
+++ contactApp/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/',include('user_app.api.urls')),
+    path('home/',include('user_app.api.urls')),
+    path('account/',include('reg.api.urls')),
 ]
